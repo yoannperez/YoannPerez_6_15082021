4fa262dcd65b9648c64e09bb77207e8dfa677b0f
Yoann Perez <yoann.perez@gmail.com>
2021-08-28 12:25:40 +0200

    pb RESOLVED

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 
 const stuffRoutes = require("./routes/stuff");
 const quizzRoutes = require("./routes/quizz");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
 mongoose
-  .connect("mongodb+srv://usertest:6pqJecu7FazQCZ7@ocfullstack.7leet.mongodb.net/OCFullstack?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect("mongodb+srv://usertest:6pqJecu7FazQCZ7@ocfullstack.7leet.mongodb.net/OCFullstack?retryWrites=true&w=majority", {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
@@ -17,10 +18,13 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   next();
 });
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/stuff", stuffRoutes);
 app.use("/api/products", quizzRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,21 @@
 const bcrypt = require("bcrypt");
+
 const User = require("../models/Users");
 
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+    
+bcrypt.hash(req.body.password, 10)
+  .then((hash) => {
+    const user = new User({
+      email: req.body.email,
+      password: hash,
+    });
+    user
+      .save()
+      .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+      .catch((error) => res.status(400).json({ error }));
+  })
+  .catch((error) => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
