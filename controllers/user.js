8168dfb8acdac9b7fd52ef89a40bb2a0b805cafd
Yoann Perez <yoann.perez@gmail.com>
2021-08-30 09:56:00 +0200

    Add dotenv

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+require ('dotenv').config();
 
 const User = require("../models/Users");
 
@@ -35,7 +36,7 @@ exports.login = (req, res, next) => {
             userId: user._id,
             token: jwt.sign(
               { userId: user._id },
-              'RANDOM_TOKEN_SECRET',
+              process.env.TOKEN_KEY,
               {expiresIn: '24h'}
               ),
           });
